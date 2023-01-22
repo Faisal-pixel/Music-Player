0b24d4e5a8622a528b33748215d8345d670b5bcb
Faisal-pixel <adamsfaisal2003@gmail.com>
2023-01-22 15:01:04 +0100

    Now users can select songs from the library, there is a bug. When the user selects a song, it doesnt play, User has to pause and play again before it starts playing.

--- src/App.js
+++ src/App.js
@@ -12,13 +12,14 @@ function App() {
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0])
   const [isPlaying, setIsPlaying] = useState(false)
+  
 
   return (
     <div className="App">
       <Song currentSong={currentSong}/>
       <Player currentSong={currentSong} isPlaying={isPlaying} setIsPlaying={setIsPlaying}/>
 
-      <Library songs={songs}/>
+      <Library songs={songs} setCurrentSong={setCurrentSong}/>
     </div>
   );
 }
--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import LibrarySong from "./LibrarySong";
 
-const Library = ({songs}) => {
+const Library = ({songs, setCurrentSong}) => {
     return(
         <div className="library">
             <h2>Library</h2>
             <div className="library-songs">
                 {
                     songs.map( song => (
-                        <LibrarySong key={song.id} song={song}/>
+                        <LibrarySong key={song.id} song={song} setCurrentSong={setCurrentSong} songs={songs}/>
                     ))
                 }
                 
